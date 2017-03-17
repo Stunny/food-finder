78589f7396e961356458b09c9a56ca974b323346
Alex Vogel <avogelm@gmail.com>
2017-03-17 11:37:49 +0100

    Android project created

---
+++ FoodFinderApp/app/src/main/java/edu/salleurl/ls30394/foodfinderapp/SplashActivity.java
@@ -0,0 +1,13 @@
+package edu.salleurl.ls30394.foodfinderapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
