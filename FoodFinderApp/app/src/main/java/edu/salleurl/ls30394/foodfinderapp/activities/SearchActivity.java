9b279552a61818897e194e2b7da40f34349c6ce4
afarre <angel_1515@hotmail.es>
2017-05-10 13:45:00 +0200

    En proceso el obtener una lectura dinamica de los kilometros de la seekBar en en layout de search. Declarada la funcion para la busqueda segun el criterio de radio y posicion del usuario.

--- FoodFinderApp/app/src/main/java/edu/salleurl/ls30394/foodfinderapp/activities/SearchActivity.java
+++ FoodFinderApp/app/src/main/java/edu/salleurl/ls30394/foodfinderapp/activities/SearchActivity.java
@@ -1,20 +1,29 @@
 package edu.salleurl.ls30394.foodfinderapp.activities;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.design.widget.TextInputLayout;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import edu.salleurl.ls30394.foodfinderapp.R;
 
 public class SearchActivity extends AppCompatActivity {
     private Intent nextActivity;
     private EditText searchField;
+    private android.widget.SeekBar seekBar;
+    private TextView seekBarValue;
+    private SeekBar.OnSeekBarChangeListener s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +45,66 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void initWidgets() {
+        seekBar = (android.widget.SeekBar) findViewById(R.id.seek_bar);
+        seekBarValue = (TextView) findViewById(R.id.search_slider_kms);
+
+        s = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                displaySeekBarValue(true);
+                Log.i("angel", "start");
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                displaySeekBarValue(false);
+                Log.i("angel", "stop");
+            }
+/*
+            private void displaySeekBarValue(boolean b) {
+                while (b){
+                    onProgressChanged(seekBar,seekBar.getProgress(), true);
+                }
+            }*/
+        };
 
     }
 
-    public void clearSearch(View view){
+    private void displaySeekBarValue(boolean b) {
+        while (b){
+            s.onProgressChanged(seekBar,seekBar.getProgress(), true);
+        }
+
+    }
+
+
+    public void clearSearch(View view) {
         searchField.setText("");
+    }
+
+    public void locationSearch(View view) {
+
+
+
+        LocationManager lm = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
     }
 
     @Override
