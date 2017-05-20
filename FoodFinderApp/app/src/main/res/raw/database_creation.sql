08703cdebfdd0a625910bd067fd1d666758c2c06
DavidGonzalezNavarro <ls30719@salle.url.edu>
2017-05-20 10:45:23 +0200

    Base de datos del restaurante añadida

--- FoodFinderApp/app/src/main/res/raw/database_creation.sql
+++ FoodFinderApp/app/src/main/res/raw/database_creation.sql
@@ -8,10 +8,24 @@ CREATE TABLE userInfo(
     description TEXT NOT NULL
 );
 
+CREATE TABLE restaurant (
+    _id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    lat FLOAT DEFAULT 0,
+    lng FLOAT DEFAULT 0,
+    address TEXT NOT NULL,
+    opening TEXT NOT NULL,--HH:MM
+    closing TEXT NOT NULL,--HH:MM
+    review FLOAT DEFAULT 0,
+    description TEXT NOT NULL,
+
+);
+
 CREATE TABLE favorite_restaurants(
     _id INTEGER PRIMARY KEY AUTOINCREMENT,
     userID INT FOREIGN KEY,
-    restaurantName TEXT NOT NULL
+    restaurantId INT FOREIGN KEY
 );
 
 CREATE TABLE recent_searches(
