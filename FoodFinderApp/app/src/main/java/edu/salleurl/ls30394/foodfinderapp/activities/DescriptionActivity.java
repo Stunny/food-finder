3159fd3e18e47cc2faa74a36d6a17d86253d7b14
Alex Vogel <avogelm@gmail.com>
2017-05-23 16:24:56 +0200

    FAB de la DescriptionActivity se inicializa segun si el restaurante ya esta en favoritos del usuario o no

--- FoodFinderApp/app/src/main/java/edu/salleurl/ls30394/foodfinderapp/activities/DescriptionActivity.java
+++ FoodFinderApp/app/src/main/java/edu/salleurl/ls30394/foodfinderapp/activities/DescriptionActivity.java
@@ -1,14 +1,10 @@
 package edu.salleurl.ls30394.foodfinderapp.activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -16,23 +12,26 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import edu.salleurl.ls30394.foodfinderapp.R;
 import edu.salleurl.ls30394.foodfinderapp.model.Restaurante;
+import edu.salleurl.ls30394.foodfinderapp.repositories.FavoriteRepo;
+import edu.salleurl.ls30394.foodfinderapp.repositories.RestaurantsRepo;
+import edu.salleurl.ls30394.foodfinderapp.repositories.UserRepo;
 import edu.salleurl.ls30394.foodfinderapp.repositories.impl.FavoriteDB;
 import edu.salleurl.ls30394.foodfinderapp.repositories.impl.RestaurantDataBase;
 import edu.salleurl.ls30394.foodfinderapp.repositories.impl.UserDatabase;
 
 public class DescriptionActivity extends AppCompatActivity {
 
-    private boolean clicked;
+    private boolean favIsClicked;
 
     private Restaurante restaurant;
     private String userName;
 
     private TextView textView,textDescription;
     private RatingBar ratingBar;
+    private FloatingActionButton fab;
 
     private Button buttonMap,buttonSend;
     private ListView listView;
@@ -49,8 +48,6 @@ public class DescriptionActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_description);
         configWitgets();
-
-
     }
 
     @Override
@@ -68,7 +65,7 @@ public class DescriptionActivity extends AppCompatActivity {
         int resId = rdb.getRestaurantId(restaurant.getName());
         int userId = udb.getUserId(userName);
 
-        if(clicked){
+        if(favIsClicked){
             if(!fdb.exists(userId, resId)){
                 fdb.addFavorite(userId, resId);
             }
@@ -96,28 +93,49 @@ public class DescriptionActivity extends AppCompatActivity {
 
         ratingBar.setRating(restaurant.getReview());
 
-        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        initFAB();
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!clicked){
+                if(!favIsClicked){
                     fab.setImageResource(R.mipmap.ic_love);
-                    clicked = true;
+                    favIsClicked = true;
                     Snackbar.make(findViewById(R.id.description_layout_parent), R.string.added_to_favs,
                             Snackbar.LENGTH_SHORT).show();
                 }else{
                     fab.setImageResource(R.mipmap.ic_not_love);
-                    clicked = false;
+                    favIsClicked = false;
                     Snackbar.make(findViewById(R.id.description_layout_parent), R.string.removed_from_favs,
                             Snackbar.LENGTH_SHORT).show();
                 }
             }
         });
+
+        // Remove focus from edit text at the creatio of the activity
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
         );
 
+    }
+
+    /**
+     * Initializes the Floating Action Button state
+     */
+    private void initFAB() {
+        RestaurantsRepo rdb = new RestaurantDataBase(this);
+        FavoriteRepo fdb = new FavoriteDB(this);
+        UserRepo udb = new UserDatabase(this);
+
+        if(!rdb.exists(restaurant.getName())){
+            fab.setImageResource(R.mipmap.ic_not_love);
+            favIsClicked = false;
+        } else if(fdb.exists(udb.getUserId(userName), rdb.getRestaurantId(restaurant.getName()))){
+            fab.setImageResource(R.mipmap.ic_love);
+            favIsClicked = true;
+        }
     }
 
     /**
